8c9dc05553fb4bcb267184e04a76e40f4e38691c
Omkar <omkar.pathade2011@gmail.com>
2023-07-24 00:38:02 +0530

    Fixed bug for Password encoder bean not found error, added registration event listener and fixed registration controller.

--- src/main/java/com/example/authentication/controller/RegistrationController.java
+++ src/main/java/com/example/authentication/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import com.example.authentication.entity.User;
 import com.example.authentication.event.RegistrationCompletionEvent;
 import com.example.authentication.model.UserModel;
 import com.example.authentication.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,9 +21,18 @@ public class RegistrationController {
     private ApplicationEventPublisher publisher;
 
     @PostMapping("/register")
-    public String registerUser(@RequestBody  UserModel userModel){
+    public String registerUser(@RequestBody  UserModel userModel, final HttpServletRequest request){
         User user = userService.registerUser(userModel);
-        publisher.publishEvent(new RegistrationCompletionEvent(user,""));
+        publisher.publishEvent(new RegistrationCompletionEvent(user,
+                applicationUrl(request)));
         return "Success";
+    }
+
+    private String applicationUrl(HttpServletRequest request) {
+        return "http://"
+                +request.getServerName()
+                +":"
+                +request.getServerPort()
+                +request.getContextPath();
     }
 }
--- src/main/java/com/example/authentication/event/listener/RegistrationEventListener.java
+++ src/main/java/com/example/authentication/event/listener/RegistrationEventListener.java
@@ -3,11 +3,13 @@ package com.example.authentication.event.listener;
 import com.example.authentication.entity.User;
 import com.example.authentication.event.RegistrationCompletionEvent;
 import com.example.authentication.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 
 import java.util.UUID;
 
+@Slf4j
 public class RegistrationEventListener implements ApplicationListener<RegistrationCompletionEvent> {
 
     @Autowired
@@ -18,5 +20,10 @@ public class RegistrationEventListener implements ApplicationListener<Registrati
         User user = event.getUser();
         String token = UUID.randomUUID().toString();
         userService.saveTokenForUser(token,user);
+
+        String url = event.getApplicationUrl()
+                +"verifyRegistration?token"
+                +token;
+        log.info("click the link to verify account {}",url);
     }
 }
